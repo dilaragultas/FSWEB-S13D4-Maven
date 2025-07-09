4aa1403177c417fde7c8d298907797dd34758729
dilaragultas <dilaragultas@gmail.com>
2025-07-09 16:35:06 +0200

    09.09.2025
    Completed

---
+++ src/main/java/org/example/Weapon.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public enum Weapon {
+    SWORD (50, 25);
+
+    private int damage;
+    private double attackSpeed;
+
+    Weapon(int damage, double attackSpeed) {
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
+
+
+}
